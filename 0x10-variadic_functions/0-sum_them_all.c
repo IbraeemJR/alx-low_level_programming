091ffff78ec63d56c28f32174900e24a554de205
@ibraeemJr <libraeem@gmail.com>
2023-07-21 00:33:31 -0700

    Solution to task 0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -8,19 +8,24 @@
  *
  * Return: If @n is 0, returns 0.
  *         Otherwise, returns the sum of all parameters.
+ *
+ * Description: This function takes a variable number of arguments and returns
+ * the sum of all the integers. It uses the va_list and va_start macros from
+ * <stdarg.h> to access the variable arguments. The sum is calculated by
+ * iterating over each argument using va_arg macro and adding them together.
  */
 int sum_them_all(const unsigned int n, ...)
 {
-    va_list args;
-    unsigned int i;
-    int sum = 0;
+	va_list args;
+	unsigned int i;
+	int sum = 0;
 
-    va_start(args, n);
+	va_start(args, n);
 
-    for (i = 0; i < n; i++)
-        sum += va_arg(args, int);
+	for (i = 0; i < n; i++)
+		sum += va_arg(args, int);
 
-    va_end(args);
+	va_end(args);
 
-    return sum;
+	return (sum);
 }
