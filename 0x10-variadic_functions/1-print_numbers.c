765b9cf6ee200246aec7cf78e86b6eeb0d05c4fc
@ibraeemJr <libraeem@gmail.com>
2023-07-21 00:34:05 -0700

    Solution to task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -7,23 +7,27 @@
  * @separator: The string to be printed between numbers. Can be NULL.
  * @n: The number of integers passed to the function.
  * @...: A variable number of integers to be printed.
+ *
+ * Description: This function takes a variable number of integers and prints
+ * them with the specified separator between them. If the separator is NULL,
+ * it will not be printed.
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-    va_list args;
-    unsigned int i;
+	va_list args;
+	unsigned int i;
 
-    va_start(args, n);
+	va_start(args, n);
 
-    for (i = 0; i < n; i++)
-    {
-        printf("%d", va_arg(args, int));
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(args, int));
 
-        if (i != (n - 1) && separator != NULL)
-            printf("%s", separator);
-    }
+		if (i != n - 1 && separator != NULL)
+			printf("%s", separator);
+	}
 
-    printf("\n");
+	printf("\n");
 
-    va_end(args);
+	va_end(args);
 }
