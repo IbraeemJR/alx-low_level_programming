70436f0a1c18456266950ace29bd42776ba574c4
@ibraeemJr <libraeem@gmail.com>
2023-08-02 02:45:46 -0700

    advanced task

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -2,51 +2,41 @@
 #include <stdlib.h>
 
 /**
- * print_opcodes - Prints the opcodes of a function.
- * @function_ptr: Pointer to the function whose opcodes to print.
- * @num_bytes: Number of bytes to print.
- */
-void print_opcodes(void *function_ptr, int num_bytes)
-{
-	unsigned char *opcodes = (unsigned char *)function_ptr;
-	int i;
-
-	for (i = 0; i < num_bytes; i++)
-	{
-		if (i == num_bytes - 1)
-			printf("%02hhx\n", opcodes[i]);
-		else
-			printf("%02hhx ", opcodes[i]);
-	}
-}
-
-/**
- * main - Prints its own opcodes.
- * @argc: Number of arguments.
- * @argv: Array of arguments.
+ * main - prints its own opcodes
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * Return: Always 0 (Success).
+ * Return: Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	int bytes;
+	int bytes, i;
+	char *arr;
 
 	if (argc != 2)
 	{
-		printf("Error: Usage: %s <number_of_bytes>\n", argv[0]);
-		return (1);
+		printf("Error\n");
+		exit(1);
 	}
 
 	bytes = atoi(argv[1]);
 
-	if (bytes <= 0)
+	if (bytes < 0)
 	{
-		printf("Error: The number of bytes must be a positive integer.\n");
-		return (2);
+		printf("Error\n");
+		exit(2);
 	}
 
-	/* Using the main function address to print its own opcodes */
-	print_opcodes((void *)main, bytes);
+	arr = (char *)main;
 
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	}
 	return (0);
 }
